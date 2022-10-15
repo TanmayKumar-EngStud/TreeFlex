aa543ca4176dbc68ad2e82d2176782408827ac62
TanmayKumar-EngStud <tanmay44a@gmail.com>
2022-10-15 17:29:29 +0530

    Branch where you can hide the nodes of tree

--- improvize.js
+++ improvize.js
@@ -18,6 +18,7 @@ button.addEventListener("click", (event) => {
  console.log(ulFinal);
  document.querySelector(".tf-tree").innerHTML = ""; // clearing up the space.
  drawingArea.append(ulFinal);
+ secondStep();
 });
 
 function commands(text) {
@@ -29,5 +30,38 @@ function commands(text) {
   mainTree.add(elem[1], elem[0]);
  }
  console.log(mainTree);
+
  return mainTree;
+}
+
+// part 2: hiding the elements.
+function secondStep() {
+ let Hider = [...document.querySelectorAll(".Hider")];
+ console.log(Hider);
+ Hider.forEach((hider) => {
+  console.log(hider);
+  hider.addEventListener("click", (event) => {
+   // getting the list of all the class names from the event.
+   let list = event.target.classList;
+   // find the ul element with the id: list[1];
+   let target = select("ul." + list[1], 0);
+   console.log(target);
+   if (getComputedStyle(target).visibility == "hidden") {
+    [...document.querySelectorAll("ul." + list[1] + " *")].forEach((elem) => {
+     if (getComputedStyle(elem).visibility == "hidden") {
+      elem.style.visibility = "visible";
+     }
+    });
+    document.querySelector("ul." + list[1]).style.visibility = "visible";
+    select(".Hider." + list[1], 0).innerHTML = "-";
+   } else {
+    // select all the elements inside and change visibility to hidden of every one of them.
+    [...document.querySelectorAll("ul." + list[1] + " *")].forEach((elem) => {
+     elem.style.visibility = "hidden";
+    });
+    document.querySelector("ul." + list[1]).style.visibility = "hidden";
+    select(".Hider." + list[1], 0).innerHTML = "+";
+   }
+  });
+ });
 }
--- tree.js
+++ tree.js
@@ -21,27 +21,24 @@ export class tree {
   return null;
  }
  static draw(Tree) {
-  // what we have to create is <ul> tag inside that there will be <li> tag.
-  // and inside the <li> tag there will be the <span.tf-nc> tag which will hold the values.
-  // we would need the bottom up approach here.
-  // create the span tag first then we will create the li tag and we will add all the elements inside that li tag.
   let span = document.createElement("span");
   span.classList.add("tf-nc");
   span.innerHTML += Tree.root;
   let li = document.createElement("li");
   li.appendChild(span); // here we have add the main node.
 
-  // now we are adding the child node.
-  // before that we know that all the child nodes must be wrapped inside the ul tag.
-  // so creating the ul tag.
-
   if (Tree.elem.length != 0) {
+   let div = document.createElement("div");
+   div.classList.add("Hider", "tf" + Tree.root);
+   div.innerHTML = "-";
    let ul = document.createElement("ul");
+   ul.classList.add("tf" + Tree.root);
    let i = 0;
    while (Tree.elem.length > i) {
     ul.appendChild(tree.draw(Tree.elem[i]));
     i++;
    }
+   span.appendChild(div);
    li.appendChild(ul);
   }
   return li;
